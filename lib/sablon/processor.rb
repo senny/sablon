acdeec888bdb98faf6c81ff744bae44a61c7e595
Yves Senn <yves.senn@gmail.com>
2014-11-17 09:21:01 +0100

    build, `to_h` is not available on older Rubies...

--- lib/sablon/processor.rb
+++ lib/sablon/processor.rb
@@ -2,7 +2,7 @@ module Sablon
   class Processor
     def self.process(xml_node, context, properties = {})
       processor = new(parser)
-      stringified_context = context.map {|k, v| [k.to_s, v] }.to_h
+      stringified_context = Hash[context.map {|k, v| [k.to_s, v] }]
       processor.manipulate xml_node, stringified_context
       processor.write_properties xml_node, properties if properties.any?
       xml_node
