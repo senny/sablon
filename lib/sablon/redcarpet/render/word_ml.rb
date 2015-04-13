50ee1207ce7f3382b51155d23044567d86920ed8
Yves Senn <yves.senn@gmail.com>
2015-04-13 17:06:29 +0200

    markdown support for ordered and unordered lists.

--- lib/sablon/redcarpet/render/word_ml.rb
+++ lib/sablon/redcarpet/render/word_ml.rb
@@ -18,7 +18,7 @@ module Sablon
 
         def normal_text(text)
           @raw_text = text
-          return '' if text.nil? || text == ''
+          return '' if text.nil? || text == '' || text == "\n"
           "<w:r><w:t xml:space=\"preserve\">#{text}</w:t></w:r>"
         end
 
@@ -28,6 +28,28 @@ module Sablon
 
         def double_emphasis(text)
           "<w:r><w:rPr><w:b /></w:rPr><w:t xml:space=\"preserve\">#{@raw_text}</w:t></w:r>"
+        end
+
+        def list(content, list_type)
+          content
+        end
+
+        LIST_PATTERN = <<-XML.gsub("\n", "")
+<w:p>
+<w:pPr>
+<w:pStyle w:val="%s" />
+</w:pPr>
+%s
+</w:p>
+XML
+        LIST_STYLE_MAPPING = {
+          ordered: "ListNumber",
+          unordered: "ListBullet"
+        }
+
+        def list_item(content, list_type)
+          list_style = LIST_STYLE_MAPPING[list_type]
+          LIST_PATTERN % [list_style, content]
         end
       end
     end
--- test/redcarpet_render_word_ml_test.rb
+++ test/redcarpet_render_word_ml_test.rb
@@ -15,6 +15,14 @@ XML
     assert_equal word_ml, @redcarpet.render("normal")
   end
 
+  def test_empty_string
+    assert_equal "", @redcarpet.render("")
+  end
+
+  def test_blank_string_with_newline
+    assert_equal "", @redcarpet.render("\n")
+  end
+
   def test_newline_in_a_paragraph_starts_new_paragraph
     word_ml = <<-XML.gsub("\n", "")
 <w:p>
@@ -54,15 +62,42 @@ XML
   def test_single_line_mixed_text
     word_ml = <<-XML.gsub("\n", "")
 <w:p>
-<w:r><w:t xml:space="preserve">some </w:t></w:r><w:r>
+
+<w:r><w:t xml:space="preserve">some </w:t></w:r>
+
+<w:r>
 <w:rPr><w:i /></w:rPr>
 <w:t xml:space="preserve">random</w:t>
-</w:r><w:r><w:t xml:space="preserve"> </w:t></w:r><w:r>
+</w:r>
+
+<w:r><w:t xml:space="preserve"> </w:t></w:r>
+<w:r>
 <w:rPr><w:b /></w:rPr>
 <w:t xml:space="preserve">text</w:t>
 </w:r>
 </w:p>
 XML
     assert_equal word_ml, @redcarpet.render("some *random* **text**")
+  end
+
+  def test_unordered_lists
+    word_ml = <<-XML.gsub("\n", "")
+<w:p>
+<w:pPr><w:pStyle w:val="ListBullet" /></w:pPr>
+<w:r><w:t xml:space="preserve">first</w:t></w:r>
+</w:p>
+
+<w:p>
+<w:pPr><w:pStyle w:val="ListBullet" /></w:pPr>
+<w:r><w:t xml:space="preserve">second</w:t></w:r>
+</w:p>
+
+<w:p>
+<w:pPr><w:pStyle w:val="ListBullet" /></w:pPr>
+<w:r><w:t xml:space="preserve">third</w:t></w:r>
+</w:p>
+XML
+
+    assert_equal word_ml, @redcarpet.render("- first\n- second\n- third")
   end
 end
