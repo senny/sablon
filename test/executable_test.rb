00ae502ee6e645480f62779369de925aa9a303da
Yves Senn <yves.senn@gmail.com>
2015-04-14 09:30:20 +0200

    rename shopping list to recipe.

--- test/executable_test.rb
+++ test/executable_test.rb
@@ -6,9 +6,10 @@ class ExecutableTest < Sablon::TestCase
   def setup
     super
     @base_path = Pathname.new(File.expand_path("../", __FILE__))
-    @output_path = @base_path + "sandbox/shopping_list.docx"
-    @template_path = @base_path + "fixtures/shopping_list_template.docx"
-    @context_path = @base_path + "fixtures/shopping_list_context.json"
+    @output_path = @base_path + "sandbox/recipe.docx"
+    @template_path = @base_path + "fixtures/recipe_template.docx"
+    @sample_path = @base_path + "fixtures/recipe_sample.docx"
+    @context_path = @base_path + "fixtures/recipe_context.json"
     @executable_path = @base_path + '../exe/sablon'
     @output_path.delete if @output_path.exist?
   end
@@ -16,12 +17,12 @@ class ExecutableTest < Sablon::TestCase
   def test_generate_document_from_template_output_to_file
     `cat #{@context_path} | #{@executable_path} #{@template_path} #{@output_path}`
 
-    assert_docx_equal @base_path + "fixtures/shopping_list_sample.docx", @output_path
+    assert_docx_equal @sample_path, @output_path
   end
 
   def test_generate_document_from_template_output_to_stdout
     `cat #{@context_path} | #{@executable_path} #{@template_path} > #{@output_path}`
 
-    assert_docx_equal @base_path + "fixtures/shopping_list_sample.docx", @output_path
+    assert_docx_equal @sample_path, @output_path
   end
 end
