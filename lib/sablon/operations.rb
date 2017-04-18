e225e6f99c51c8a2fd41a4d3b137fd620cc6e449
Yves Senn <yves.senn@gmail.com>
2017-04-18 17:48:29 +0200

    remove double evaluation.

--- lib/sablon/operations.rb
+++ lib/sablon/operations.rb
@@ -4,7 +4,7 @@ module Sablon
     class Insertion < Struct.new(:expr, :field)
       def evaluate(context)
         if content = expr.evaluate(context)
-          field.replace(Sablon::Content.wrap(expr.evaluate(context)))
+          field.replace(Sablon::Content.wrap(content))
         else
           field.remove
         end
