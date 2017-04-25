32f5c9b0784b5500a3f2273376905c29ccb129fe
Matthew Stadelman <mstadelman@mitre.org>
2017-04-25 09:39:54 -0400

    Rename test class ContextTest -> EnvironmentTest

--- test/sandbox/environment_test.rb
+++ test/sandbox/environment_test.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 require "test_helper"
 
-class ContextTest < Sablon::TestCase
+class EnvironmentTest < Sablon::TestCase
   def test_converts_symbol_keys_to_string_keys
     env = Sablon::Environment.new(nil, {a: 1, b: {c: 2, "d" => 3}})
     assert_equal({"a"=>1, "b"=>{"c" =>2, "d"=>3}}, env.context)
