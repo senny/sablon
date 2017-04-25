af4d5bf063c7b1765732fcf6dd4373bec4d9b876
Matthew Stadelman <mstadelman@mitre.org>
2017-04-25 09:38:50 -0400

    Fix tests in context_test.rb

--- test/context_test.rb
+++ test/context_test.rb
@@ -3,26 +3,26 @@ require "test_helper"
 
 class ContextTest < Sablon::TestCase
   def test_converts_symbol_keys_to_string_keys
-    context = Sablon::Context.new(nil, {a: 1, b: {c: 2, "d" => 3}})
-    assert_equal({"a"=>1, "b"=>{"c" =>2, "d"=>3}}, context)
+    env = Sablon::Environment.new(nil, {a: 1, b: {c: 2, "d" => 3}})
+    assert_equal({"a"=>1, "b"=>{"c" =>2, "d"=>3}}, env.context)
   end
 
   def test_recognizes_wordml_keys
-    context = Sablon::Context.new(nil, {"word_ml:mykey" => "<w:p><w:p>", "otherkey" => "<nope>"})
+    env = Sablon::Environment.new(nil, {"word_ml:mykey" => "<w:p><w:p>", "otherkey" => "<nope>"})
     assert_equal({ "mykey"=>Sablon.content(:word_ml, "<w:p><w:p>"),
-                   "otherkey"=>"<nope>"}, context)
+                   "otherkey"=>"<nope>"}, env.context)
   end
 
   def test_recognizes_html_keys
-    context = Sablon::Context.new(nil, {"html:mykey" => "**yay**", "otherkey" => "<nope>"})
+    env = Sablon::Environment.new(nil, {"html:mykey" => "**yay**", "otherkey" => "<nope>"})
     assert_equal({ "mykey"=>Sablon.content(:html, "**yay**"),
-                   "otherkey"=>"<nope>"}, context)
+                   "otherkey"=>"<nope>"}, env.context)
   end
 
   def test_does_not_wrap_html_and_wordml_with_nil_value
-    context = Sablon::Context.new(nil, {"html:mykey" => nil, "word_ml:otherkey" => nil, "normalkey" => nil})
+    env = Sablon::Environment.new(nil, {"html:mykey" => nil, "word_ml:otherkey" => nil, "normalkey" => nil})
     assert_equal({ "mykey" => nil,
                    "otherkey" => nil,
-                   "normalkey" => nil}, context)
+                   "normalkey" => nil}, env.context)
   end
 end
