bd7f9f4c0b02b2a4e13e611acac2d11211b55c5f
Matthew Stadelman <mstadelman@mitre.org>
2017-05-03 14:35:09 -0400

    Move hash transformation methosds back into a Context module

--- lib/sablon/environment.rb
+++ lib/sablon/environment.rb
@@ -1,4 +1,38 @@
 module Sablon
+  # manipulates use supplied hash maps to be suitable for content replacement
+  # inside a docx template file.
+  module Context
+    class << self
+      def transform_hash(hash)
+        Hash[hash.map { |k, v| transform_pair(k.to_s, v) }]
+      end
+
+      private
+
+      def transform_standard_key(key, value)
+        case value
+        when Hash
+          [key, transform_hash(value)]
+        else
+          [key, value]
+        end
+      end
+
+      def transform_pair(key, value)
+        if key =~ /\A([^:]+):(.+)\z/
+          if value.nil?
+            [Regexp.last_match[2], value]
+          else
+            key_sym = Regexp.last_match[1].to_sym
+            [Regexp.last_match[2], Sablon.content(key_sym, value)]
+          end
+        else
+          transform_standard_key(key, value)
+        end
+      end
+    end
+  end
+
   # Combines the user supplied context and template into a single object
   # to manage data during template processing.
   class Environment
@@ -15,33 +49,7 @@ module Sablon
 
     def initialize(template, context = {})
       @template = template
-      @context = transform_hash(context)
-    end
-
-    def transform_hash(hash)
-      Hash[hash.map { |k, v| transform_pair(k.to_s, v) }]
-    end
-
-    def transform_standard_key(key, value)
-      case value
-      when Hash
-        [key, transform_hash(value)]
-      else
-        [key, value]
-      end
-    end
-
-    def transform_pair(key, value)
-      if key =~ /\A([^:]+):(.+)\z/
-        if value.nil?
-          [Regexp.last_match[2], value]
-        else
-          key_sym = Regexp.last_match[1].to_sym
-          [Regexp.last_match[2], Sablon.content(key_sym, value)]
-        end
-      else
-        transform_standard_key(key, value)
-      end
+      @context = Context::transform_hash(context)
     end
   end
 end
