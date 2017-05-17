56da37eb7cacf5b44542a02de82d5adfce4d47ee
Matthew Stadelman <mstadelman@mitre.org>
2017-05-17 11:16:51 -0400

    Update Template.render to include relationship processing

--- lib/sablon/template.rb
+++ lib/sablon/template.rb
@@ -22,6 +22,7 @@ module Sablon
       env = Sablon::Environment.new(self, context)
       Zip.sort_entries = true # required to process document.xml before numbering.xml
       Zip::OutputStream.write_buffer(StringIO.new) do |out|
+        env.relationships.initialize_rids(@path)
         Zip::File.open(@path).each do |entry|
           entry_name = entry.name
           out.put_next_entry(entry_name)
@@ -34,16 +35,15 @@ module Sablon
             out.write(process(Processor::Document, content, env))
           elsif entry_name == 'word/numbering.xml'
             out.write(process(Processor::Numbering, content, env))
-          elsif entry_name == 'word/_rels/document.xml.rels'
-            out.write(process(Processor::Image, content, env))
           elsif entry_name == '[Content_Types].xml'
             out.write(process(Processor::ContentType, content, properties, out))
           else
             out.write(content)
           end
         end
-        # finally add all used images to the zip file
+        # finally add all used images to the zip file and update relationships
         env.images.add_images_to_zip!(out)
+        env.relationships.output_new_rids(@path, out)
       end
     end
 
