e583ff4c2714a919bfba93610c71a18a6870ec4c
Matthew Stadelman <mstadelman@mitre.org>
2017-05-19 14:02:53 -0400

    Clean up processor/content_type.rb
    Removed extra args used in self.process, as well as various minor
    rubocop fixes.

--- lib/sablon/processor/content_type.rb
+++ lib/sablon/processor/content_type.rb
@@ -1,28 +1,30 @@
-module Sablon::Processor::ContentType
-  TYPES = {
-    png: 'image/png',
-    jpg: 'image/jpeg',
-    jpeg: 'image/jpeg',
-    gif: 'image/gif',
-    bmp: 'image/bmp'
-  }
+module Sablon
+  module Processor
+    # Adds supported content types to the docx file
+    class ContentType
+      TYPES = {
+        png: 'image/png',
+        jpg: 'image/jpeg',
+        jpeg: 'image/jpeg',
+        gif: 'image/gif',
+        bmp: 'image/bmp'
+      }.freeze
 
-  def self.process(doc, properties, out)
-    TYPES.each do |extension, content_type|
-      unless extensions(doc).include?(extension.to_s)
-        node = Nokogiri::XML::Node.new('Default', doc)
-        node['Extension'] = extension
-        node['ContentType'] = content_type
-        doc.root << node
+      def self.process(doc)
+        TYPES.each do |extension, content_type|
+          next if extensions(doc).include?(extension.to_s)
+          node = Nokogiri::XML::Node.new('Default', doc)
+          node['Extension'] = extension
+          node['ContentType'] = content_type
+          doc.root << node
+        end
+
+        doc
+      end
+
+      def self.extensions(doc)
+        doc.root.children.map { |child| child['Extension'] }.compact.uniq
       end
     end
-
-    doc
-  end
-
-  private
-
-  def self.extensions(doc)
-    doc.root.children.map{ |child| child['Extension'] }.compact.uniq
   end
 end
