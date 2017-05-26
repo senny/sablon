9b32a965c0dbf6a498e52d3e8b383ae02dc9aeb3
Matthew Stadelman <mstadelman@mitre.org>
2017-05-26 07:45:36 -0400

    Merge changes in upstream into feature
    Squashed commit of the following:
    
    commit 6ca2454818688c2cf94bd3ec1b302d4b15cbcffc
    Author: Yves Senn <yves.senn@gmail.com>
    Date:   Fri May 26 11:28:43 2017 +0200
    
        refactor, no need to explicitly reference self.
    
    commit ae2c1613ae5029642aad4aa1f5e941e8678c00f6
    Author: Yves Senn <yves.senn@gmail.com>
    Date:   Fri May 26 11:28:09 2017 +0200
    
        refactor, put NodeProperties into separate test case.
    
    commit e8e77db274e19375e6215dd121dd599cb2795ffe
    Author: Matthew Stadelman <stadelmanma@gmail.com>
    Date:   Fri May 26 05:26:18 2017 -0400
    
        Support arbitrary node attributes on paragraphs (#51)
    
        * Pass node diretly into Paragraph.new
    
        * Pass definition directly into ListParagraph.new
    
        * Change argument order for Paragraph.new and ListParagraph.new
    
        * Refactor some of the logic in ast_next_paragraph into a prepare_node fucntion
    
        This function wil later on ensure all nodes have a stype attribute
        even if it is an empty string to avoid nil's popping up.
    
        * Use node['class'] instead of directly passing the Paragraph style in
    
        * Change node['class'] to node['pStyle']
    
        This will work better once I get some of the attribute logic working.
    
        * Implement simple parsing of attributes
    
        * Implement parsing of nested attributes
    
        This also removes the special case of ListParagraph
    
        * Remove ListParagraph class
    
        * Refactor list testing methods to handle direct usage of pPr to store list props
    
        * Fix comment in ast.rb
    
        * Refactor pattern to_docx usage
    
        * Rename prepare_node to prepare_paragraph
    
        * Return properties from prepare_paragraph and pass onto Paragraph initialization
    
        * Remove passing of node into Parapgrah, only properities is needed
    
        * Rename Node.process_attributes into Node.process_properties
    
        It also uses the @properties isntance var instead of @attributes
    
        * Fix html converter test to use new instance var
    
        * Rewrite transform_attr to work on arrays with nested hashes
    
        This replaces the much more fragile RegExp logic in place before.
    
        * Change numPr property defintion to be a nested list of hashes
    
        This was done to match the new interface expected by transform_attr
    
        * Rubocop fix, curly bracket spacing
    
        * Update converter test to check nested property structure instead of using regexp
    
        * Fix syntax error leftover from rebase
    
        accidently killed off an `end` tag
    
        * Refactor Node.process_properties method
    
        * Slight change in default handling of node properties when value is a scalar
    
        * slight refactor of Pararaph.ppr_docx and Paragraph.to_docx
    
        * Move logic to handle properties from Node into a NodeProperties class
    
        * Minor refactor to better handle proper hashes that have keys with nil value
    
        * Implement unit tets fot NodeProperties class
    
        * Refactor to pass tagname in during instantiation so to_docx call signature is consistent
    
        * Split up node properties test method into smaller tests cases
    
        * Add paragraph factory method to node properties

--- lib/sablon/html/ast.rb
+++ lib/sablon/html/ast.rb
@@ -9,26 +9,57 @@ module Sablon
       def self.node_name
         @node_name ||= name.split('::').last
       end
+    end
+
+    class NodeProperties
+      def self.paragraph(properties)
+        new('w:pPr', properties, Paragraph::PROPERTIES)
+      end
+
+      def self.run(properties)
+        new('w:rPr', properties, Run::PROPERTIES)
+      end
+
+      def initialize(tagname, properties, whitelist)
+        @tagname = tagname
+        @properties = filter(properties, whitelist)
+      end
+
+      def inspect
+        @properties.map { |k, v| v ? "#{k}=#{v}" : k }.join(';')
+      end
+
+      def [](key)
+        @properties[key]
+      end
+
+      def []=(key, value)
+        @properties[key] = value
+      end
+
+      def to_docx
+        "<#{@tagname}>#{process}</#{@tagname}>" unless @properties.empty?
+      end
 
       private
 
-      def filter_properties(properties)
+      def filter(properties, whitelist)
         props = properties.map do |key, value|
-          next unless self.class::PROPERTIES.include? key
+          next unless whitelist.include? key
           [key, value]
         end
         # filter out nils and return hash
-        Hash[props.reject { |pair| pair.nil? }]
+        Hash[props.compact]
       end
 
       # processes attributes defined on the node into wordML property syntax
-      def process_properties
+      def process
         @properties.map { |k, v| transform_attr(k, v) }.join
       end
 
       # properties that have a list as the value get nested in tags and
       # each entry in the list is transformed. When a value is a hash the
-      # keys in the hash are used to explicitly buld the XML tag attributes.
+      # keys in the hash are used to explicitly build the XML tag attributes.
       def transform_attr(key, value)
         if value.is_a? Array
           sub_attrs = value.map do |sub_prop|
@@ -36,8 +67,8 @@ module Sablon
           end
           "<w:#{key}>#{sub_attrs.join}</w:#{key}>"
         elsif value.is_a? Hash
-          props = value.map { |k, v| format('w:%s="%s"', k, v) }
-          "<w:#{key} #{props.join(' ')} />"
+          props = value.map { |k, v| format('w:%s="%s"', k, v) if v }
+          "<w:#{key} #{props.compact.join(' ')} />"
         else
           value = format('w:val="%s" ', value) if value
           "<w:#{key} #{value}/>"
@@ -84,13 +115,14 @@ module Sablon
                       outlineLvl pBdr pStyle rPr sectPr shd spacing
                       tabs textAlignment].freeze
       attr_accessor :runs
+
       def initialize(properties, runs)
-        @properties = filter_properties(properties)
+        @properties = NodeProperties.paragraph(properties)
         @runs = runs
       end
 
       def to_docx
-        "<w:p>#{ppr_docx}#{runs.to_docx}</w:p>"
+        "<w:p>#{@properties.to_docx}#{runs.to_docx}</w:p>"
       end
 
       def accept(visitor)
@@ -101,12 +133,6 @@ module Sablon
       def inspect
         "<Paragraph{#{@properties['pStyle']}}: #{runs.inspect}>"
       end
-
-      private
-
-      def ppr_docx
-        "<w:pPr>#{process_properties}</w:pPr>"
-      end
     end
 
     class Run < Node
@@ -114,25 +140,22 @@ module Sablon
                       rStyle shadow shd smallCaps strike sz u vanish
                       vertAlign].freeze
       attr_reader :string
+
       def initialize(string, properties)
-        @properties = filter_properties(properties)
+        @properties = NodeProperties.run(properties)
         @string = string
       end
 
       def to_docx
-        "<w:r>#{rpr_docx}#{text}</w:r>"
+        "<w:r>#{@properties.to_docx}#{text}</w:r>"
       end
 
       def inspect
-        rpr_str = @properties.map { |k, v| v ? "#{k}=#{v}" : k }.join(';')
-        "<Run{#{rpr_str}}: #{string}>"
+        "<Run{#{@properties.inspect}}: #{string}>"
       end
 
       private
 
-      def rpr_docx
-        "<w:rPr>#{process_properties}</w:rPr>" unless @properties.empty?
-      end
 
       def text
         content = @string.tr("\u00A0", ' ')
