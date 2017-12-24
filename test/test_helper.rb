daa40104d75c2a1acc07398f0ff538f7bd8087db
Ahmed Wahba <ahmed.el-mahalawey@radius60.com>
2017-12-24 13:44:29 +0200

    Also, we will need a matching assertion in test/html/converter_test.rb that checks the XML generated.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,4 +14,20 @@ class Sablon::TestCase < MiniTest::Test
   def teardown
     super
   end
+
+  class UIDTestGenerator
+    def initialize
+      @current_id = rand(10...100)
+      @current_id_start = @current_id
+    end
+
+    def new_uid
+      @current_id += 1
+      @current_id.to_s
+    end
+
+    def reset
+      @current_id = @current_id_start
+    end
+  end
 end
