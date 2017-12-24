f3058832ffe5aa3c1bb9753f348b44e74edb1670
Ahmed Wahba <ahmed.el-mahalawey@radius60.com>
2017-12-24 22:17:59 +0200

    Generating html.docx by sablon and replacing it with html_sample.docx file

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,8 +17,7 @@ class Sablon::TestCase < MiniTest::Test
 
   class UIDTestGenerator
     def initialize
-      # @current_id = rand(10...100)
-      @current_id = 122
+      @current_id = 1234
       @current_id_start = @current_id
     end
 
