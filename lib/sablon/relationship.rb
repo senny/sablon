125f28ed3785569b0f0fbf98d99a44a28c527fe1
Matthew Stadelman <stadelmanma@gmail.com>
2017-12-26 13:29:53 -0500

    Code style refactors in relationships.rb

--- lib/sablon/relationship.rb
+++ lib/sablon/relationship.rb
@@ -1,8 +1,7 @@
 module Sablon
-  # Handles storing referenced relationships in the document.xml file and writing them
-  # down in the document.xml.rels file
+  # Handles storing referenced relationships in the document.xml file and
+  # writing them to the document.xml.rels file
   class Relationship
-
     attr_accessor :relationships
 
     def initialize
@@ -11,30 +10,38 @@ module Sablon
 
     def add_found_relationships(content, output_stream)
       output_stream.put_next_entry('word/_rels/document.xml.rels')
-      if @relationships.length > 0
+      #
+      unless @relationships.empty?
         rels_doc = Nokogiri::XML(content)
         rels_doc_root = rels_doc.root
-        node_set = Nokogiri::XML::NodeSet.new(rels_doc)
-        @relationships.each do |relationship|
-          relationship_tag = "<Relationship#{relationship_attributes(relationship)}/>"
-          node_set << Nokogiri::XML.fragment(relationship_tag).children.first
-        end
-
-        rels_doc_root.last_element_child.after(node_set)
-
-        output_stream.write(rels_doc.to_xml(indent: 0, save_with: 0))
+        # convert new rels to nodes
+        node_set = convert_relationships_to_node_set(rels_doc)
         @relationships = []
-      else
-        output_stream.write(content)
+        # add new nodes to XML content
+        rels_doc_root.last_element_child.after(node_set)
+        content = rels_doc.to_xml(indent: 0, save_with: 0)
       end
+      #
+      output_stream.write(content)
     end
 
     private
 
+    # Builds a set of Relationship XML nodes from the stored relationships
+    def convert_relationships_to_node_set(doc)
+      node_set = Nokogiri::XML::NodeSet.new(doc)
+      @relationships.each do |relationship|
+        rel_tag = "<Relationship#{relationship_attributes(relationship)}/>"
+        node_set << Nokogiri::XML.fragment(rel_tag).children.first
+      end
+      #
+      node_set
+    end
+
+    # Builds the attribute string for the relationship XML node
     def relationship_attributes(relationship)
       return '' if relationship.nil? || relationship.empty?
       ' ' + relationship.map { |k, v| %(#{k}="#{v}") }.join(' ')
     end
-
   end
 end
