df1c16977d0db80c773d6dbf74bc003a2309d21a
Matthew Stadelman <stadelmanma@gmail.com>
2018-01-10 09:40:49 -0500

    Create initial version of new docx equality assertion
    I now need to inspect more files than just the document.xml since
    other content such as headers, footers, and *.rels files may be
    modified during the run. Additionally, moving the assertion into
    the Minitest::Assertions module gives me access to a tool to show
    a diff of the extracted XML. This is useful for small changes in files.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,7 @@ SimpleCov.start do
 end
 
 require "bundler/setup"
+require 'minitest/assertions'
 require "minitest/autorun"
 require "minitest/mock"
 require "xmlsimple"
@@ -18,7 +19,45 @@ end
 
 $: << File.expand_path('../../lib', __FILE__)
 require "sablon"
-require "sablon/test"
+
+module Minitest
+  module Assertions
+    def assert_docx_equal(expected_path, actual_path)
+      msg = <<-MSG.gsub(/^ +/, '')
+        The generated document does not match the sample. Please investigate file(s): %s.
+
+        If the generated document is correct, the sample needs to be updated:
+        \t cp #{actual_path} #{expected_path}
+      MSG
+      #
+      expected_contents = parse_docx(expected_path)
+      actual_contents = parse_docx(actual_path)
+      #
+      mismatch = []
+      expected_contents.each do |entry_name, exp_cnt|
+        next unless exp_cnt != actual_contents[entry_name]
+        mismatch << entry_name
+        next unless entry_name =~ /.xml$|.rels$/
+        puts diff Nokogiri::XML(exp_cnt).to_xml, Nokogiri::XML(actual_contents[entry_name]).to_xml
+      end
+      #
+      msg = format(msg, mismatch.join(', '))
+      raise Minitest::Assertion, msg unless mismatch.empty?
+    end
+
+    def parse_docx(path)
+      contents = {}
+      Zip::File.open(path) do |zip_file|
+        zip_file.each do |entry|
+          next unless entry.file?
+          contents[entry.name] = entry.get_input_stream.read
+        end
+      end
+      #
+      contents
+    end
+  end
+end
 
 class Sablon::TestCase < MiniTest::Test
   def teardown
