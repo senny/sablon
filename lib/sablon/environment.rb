b6b51582d0eeaac0dcefd01b0eb3f14a9a206111
Matthew Stadelman <stadelmanma@gmail.com>
2018-01-11 07:37:31 -0500

    Remove logic that allowed passing in of a parent environment to Environment#initialize
    This logic was needed because I had to pass in things like numbering and
    relationships directly but those are now handled by the DOM which is
    pulled from the template.

--- lib/sablon/environment.rb
+++ lib/sablon/environment.rb
@@ -8,7 +8,7 @@ module Sablon
     # returns a new environment with merged contexts
     def alter_context(context = {})
       new_context = @context.merge(context)
-      Environment.new(nil, new_context, self)
+      Environment.new(template, new_context)
     end
 
     # reader method for the DOM::Model instance stored on the template
@@ -18,13 +18,8 @@ module Sablon
 
     private
 
-    def initialize(template, context = {}, parent_env = nil)
-      # pass attributes of the supplied environment to the new one or
-      # create new references
-
-      # I can replace this by just always passing in a valid template
-      # in alter_context
-      @template = parent_env ? parent_env.template : template
+    def initialize(template, context = {})
+      @template = template
       @context = Context.transform_hash(context)
     end
   end
